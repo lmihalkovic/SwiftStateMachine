be1db72aeff6ee5916ce4af71d9bd316577091a1
L Mihalkovic <laurentm@codingblues.io>
2019-06-13 16:40:14 +0200

    recompiled in 10.xx

--- StateMachinePlayground.playground/Contents.swift
+++ StateMachinePlayground.playground/Contents.swift
@@ -3,36 +3,35 @@
 import Cocoa
 
 // -------------------------------------------------------------------
-public enum TransitionError : ErrorType {
+public enum TransitionError : Error {
     //TODO: deal with errors
 }
 
-public typealias TransitionHandler = (data:Any?) -> ()
+public typealias TransitionHandler = (_ data: Any?) -> ()
 
 public class State<T:Hashable> : CustomDebugStringConvertible {
     let state:T
-    required public init(_ data:T) {
+    required public init(_ data: T) {
         self.state = data
     }
     public var debugDescription:String {
-        return "[state:\(state)]"
+        return "[state: \(state)]"
     }
 }
 
-struct Transition<T:Hashable> : CustomDebugStringConvertible {
+struct Transition<T: Hashable> : CustomDebugStringConvertible {
     let from:State<T>?
     let to:State<T>?
     var handler:TransitionHandler?
     
     var debugDescription:String {
-        return "[\(from)->\(to): \(handler)]"
+        return "[\(String(describing: from))->\(String(describing: to)): \(String(describing: handler))]"
     }
-    
 }
 
-public class StateMachine<T:Hashable> : CustomPlaygroundQuickLookable{
+public class StateMachine<T:Hashable> : CustomPlaygroundDisplayConvertible {
     
-    var state:State<T>?
+    var state: State<T>?
     var transitions:[Transition<T>] = []
     
     required public init() {
@@ -41,42 +40,42 @@ public class StateMachine<T:Hashable> : CustomPlaygroundQuickLookable{
         self.state = State(initialState)
     }
     
-    public func onEnter(state:T, handler:TransitionHandler) {
+    public func onEnter(_ state:T, handler:@escaping TransitionHandler) {
         onEnter(State(state), handler: handler)
     }
     
-    func onEnter(state:State<T>, handler:TransitionHandler) {
-        let t = Transition(from:nil, to:state, handler:handler)
+    func onEnter(_ state: State<T>, handler:@escaping TransitionHandler) {
+        let t = Transition(from:nil, to:state, handler: handler)
         transitions.append(t)
     }
     
-    public func onLeave(state:T, handler:TransitionHandler) {
+    public func onLeave(_ state: T, handler:@escaping TransitionHandler) {
         onLeave(State(state), handler: handler)
     }
     
-    public func onLeave(state:State<T>, handler:TransitionHandler) {
-        let t = Transition(from:state, to:nil, handler:handler)
+    public func onLeave(_ state: State<T>, handler:@escaping TransitionHandler) {
+        let t = Transition(from:state, to: nil, handler: handler)
         transitions.append(t)
     }
     
-    public func transitionTo(state:T) {
+    public func transitionTo(_ state:T) {
         transitionTo(State(state), data: nil)
     }
-    func transitionTo(state:State<T>) {
+    func transitionTo(_ state: State<T>) {
         transitionTo(state, data: nil)
     }
-    public func transitionTo(state:T, data:Any?) {
+    public func transitionTo(_ state: T, data: Any?) {
         transitionTo(State(state), data: data)
     }
     
-    func transitionTo(state:State<T>, data:Any?) {
+    func transitionTo(_ state: State<T>, data: Any?) {
         if let current = self.state {
-            do {
-                try? performTransition(current, to: state, data: data)
-                self.state = state
-            } catch {
-                //TODO: errors
-            }
+            try? performTransition(from: current, to: state, data: data)
+            self.state = state
+//            do {
+//            } catch {
+//                //TODO: errors
+//            }
         } else {
             
         }
@@ -86,23 +85,23 @@ public class StateMachine<T:Hashable> : CustomPlaygroundQuickLookable{
         return state
     }
     
-    func performTransition(from:State<T>, to:State<T>, data:Any?) throws {
+    func performTransition(from: State<T>, to: State<T>, data: Any?) throws {
         let source = transitions.filter { (t) in
             return t.from?.state == from.state
         }
         let target = transitions.filter { (t) in
             return t.to?.state == to.state
         }
-        source.forEach { $0.handler?(data: data) }
-        target.forEach { $0.handler?(data: data) }
+        source.forEach { $0.handler?(data) }
+        target.forEach { $0.handler?(data) }
     }
     
-    public func customPlaygroundQuickLook() -> PlaygroundQuickLook {
+    public var playgroundDescription: Any {
         var s = ""
         for t in self.transitions {
-            s += (t.debugDescription + " ")
+            s += (t.debugDescription + " \n")
         }
-        return .Text("[\(s)]")
+        return String("[\(s)]")
     }
 }
 
@@ -118,13 +117,13 @@ public enum GameState {
 
 var sm = StateMachine(GameState.UNINITIALIZED)
 sm.onEnter(.INITIALIZE) { (data) in
-    print("init1: \(data)")
+    print("init1: \(String(describing: data))")
 }
 sm.onEnter(.INITIALIZE) { (data) in
-    print("init2: \(data)")
+    print("init2: \(String(describing: data))")
 }
 sm.onEnter(.USER_PLAY) { (data) in
-    print("USER_PLAY \(data)")
+    print("USER_PLAY \(String(describing: data))")
 }
 
 sm.transitionTo(.INITIALIZE, data:"TEST")
